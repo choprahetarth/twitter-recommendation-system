d978e34c23180fa4e7b68dab66c402e89784e635
Hetarth Chopra <34271010+choprahetarth@users.noreply.github.com>
2023-01-18 00:53:53 +0530

    debugged

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,8 @@ def semantic_s(query, corpus_embeddings, dataset,embedder):
         if i>0:
             st.write(dataset[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
 
-st.text('Please select a random tweet and get it's recommended tweets')
+st.text('Please select a random tweet and get it
+s recommended tweets')
 
 
 
